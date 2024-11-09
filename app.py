e875be678aad0163c007bfa50ea8b160a58b089d
Мега Кокос <ermolav2011+THK@ya.ru>
2024-11-09 12:57:25 +0300

    Добавил заглушки

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+import time
+
 def default_function_first():
     """Return default plain-text"""
     return "Hello, i'm python web app"
+
+
+def check_title_function(title: str):
+    """Проверка наименования"""
+    print(f"title: {title}")
+    time.sleep(4.5)
+    return {100, "Проверка наименования выполнена успешно"}
+
+
+def check_contract_enforced_function(contract_enforced: str):
+    """Проверка обеспечения исполнения контракта"""
+    print(f"contract_enforced: {contract_enforced}")
+    time.sleep(4.5)
+    return {100, "Проверка обеспечения исполнения контракта выполнена успешно"}
+
+
+def check_photo_function(photo_url: str):
+    """Проверка фото"""
+    print(f"photo_url: {photo_url}")
+    time.sleep(4.5)
+    return {100, "Проверка фото выполнена успешно"}
--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import asyncio
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 import uvicorn
 
-from app import default_function_first
+from app import *
 
 app = FastAPI()
 
 
 @app.get("/api/")
-def default_endpoint():
+async def default_endpoint():
     """Этот эндпоинт просто возвращает текст по запросу"""
     return default_function_first()
+
+
+@app.post("/api/check_title")
+async def check_title(data=Body()):
+    """Проверка наименования"""
+    input_title = data.title
+    return check_title_function(input_title)
+
+
+@app.post("/api/check_contract_enforced")
+async def check_contract_enforced(data=Body()):
+    """Проверка наименования"""
+    contract_enforced = data.contract_enforced
+    return check_contract_enforced_function(contract_enforced)
+
+
+@app.post("/api/check_photo")
+async def check_photo(data=Body()):
+    """Проверка наименования"""
+    photo_url = data.photo_url
+    return check_photo_function(photo_url)
+
 
 
 async def run():
